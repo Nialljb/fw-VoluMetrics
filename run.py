7ba5976e706a6cb36058b0c14ef3f1c5e342aa9a
Hajer <hajer@Nialls-MacBook-Pro.local>
2024-10-11 18:34:59 +0100

    Recoded report layout using reportlab. Changed interactive.sh to take image tag and job log for flexibility

--- run.py
+++ run.py
@@ -24,20 +24,22 @@ log = logging.getLogger(__name__)
 # Define the main function
 def main(context: GearToolkitContext) -> None:
 
+    output_dir= "/flywheel/v0/work/"
+
     # Step 0: Parse the configuration file
     user, filepath, input_label, age_min, age_max, threshold, project_label = parse_config(context)
 
     # Step 1: Create the cover page
-    cover = create_cover_page(user, input_label, age_min, age_max, threshold, project_label)
+    cover = create_cover_page(user, input_label, age_min, age_max, threshold, project_label,output_dir)
 
     # Step 2: Parse the CSV file
     df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels = parse_csv(filepath, project_label, age_min, age_max, threshold)
 
     # Step 3: Create the data report
-    report = create_data_report(df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels, age_min, age_max, threshold)
+    report = create_data_report(df, summary_table, filtered_df, n, n_projects, n_sessions, n_clean_sessions, outlier_n, project_labels, labels, age_min, age_max, threshold,output_dir)
 
     # Step 4: Merge cover page and data report
-        # Get the current timestamp
+    # Get the current timestamp
     current_timestamp = datetime.now()
     # Format the timestamp as a string
     formatted_timestamp = current_timestamp.strftime('%Y-%m-%d_%H-%M-%S')
